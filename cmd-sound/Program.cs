9485a928fd8d6fe2ea6d18539aaa1ae395d20a7e
Subhajit Sahu <wolfram77@gmail.com>
2016-07-26 23:26:04 +0530

    program: created
    uses milliseconds
    name updated

--- cmd-sound/Program.cs
+++ cmd-sound/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
-namespace cmd_sound {
+namespace orez.osound {
 	class Program {
+
+		// constant data
+		/// <summary>
+		/// Minimum producable frequency.
+		/// </summary>
+		private const int FREQ_MIN = 37;
+		/// <summary>
+		/// Maximum producable frequency.
+		/// </summary>
+		private const int FREQ_MAX = 32767;
+
+
+		// static method
+		/// <summary>
+		/// My Heart Will Go On
+		/// : James Horner, Celine Dion
+		/// </summary>
+		/// <param name="args">Input arguments.</param>
 		static void Main(string[] args) {
+			oParams p = new oParams(args);
+			if (p.Time <= 0) return;
+			if (p.Freq < FREQ_MIN || p.Freq > FREQ_MAX) Thread.Sleep(p.Time);
+			else Console.Beep(p.Freq, p.Time);
 		}
 	}
 }
---
+++ cmd-sound/oParams.cs
@@ -0,0 +1,28 @@
+﻿namespace orez.osound {
+	class oParams {
+
+		// data
+		/// <summary>
+		/// Frequency of sound to produce.
+		/// </summary>
+		public int Freq;
+		/// <summary>
+		/// Duration of sound in milliseconds.
+		/// </summary>
+		public int Time;
+
+
+		// constructor
+		/// <summary>
+		/// Get parameters from input arguments.
+		/// </summary>
+		/// <param name="a">Input arguments.</param>
+		public oParams(string[] a) {
+			double n = 0;
+			for (int i = 0; i < a.Length; i++) {
+				if (i == 0) { double.TryParse(a[i], out n); Freq = (int)n; }
+				else if (i == 1) { double.TryParse(a[i], out n); Time = (int)n; }
+			}
+		}
+	}
+}
