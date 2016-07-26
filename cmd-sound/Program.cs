f7857e1c5b2e58e920a0377834e6354b81c8b992
Subhajit Sahu <wolfram77@gmail.com>
2016-07-26 23:26:04 +0530

    program: created (uses milliseconds)

--- cmd-sound/Program.cs
+++ cmd-sound/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace cmd_sound {
 	class Program {
+
+		// static method
+		/// <summary>
+		/// My Heart Will Go On
+		/// : James Horner, Celine Dion
+		/// </summary>
+		/// <param name="args">Input arguments.</param>
 		static void Main(string[] args) {
+			oParams p = new oParams(args);
+			if (p.Freq <= 0 || p.Time <= 0) return;
+			Console.Beep((int)p.Freq, (int)p.Time);
 		}
 	}
 }
---
+++ cmd-sound/oParams.cs
@@ -0,0 +1,27 @@
+﻿namespace cmd_sound {
+	class oParams {
+
+		// data
+		/// <summary>
+		/// Frequency of sound to produce.
+		/// </summary>
+		public double Freq;
+		/// <summary>
+		/// Duration of sound in milliseconds.
+		/// </summary>
+		public double Time;
+
+
+		// constructor
+		/// <summary>
+		/// Get parameters from input arguments.
+		/// </summary>
+		/// <param name="a">Input arguments.</param>
+		public oParams(string[] a) {
+			for (int i = 0; i < a.Length; i++) {
+				if (i == 0) double.TryParse(a[i], out Freq);
+				else if (i == 1) double.TryParse(a[i], out Time);
+			}
+		}
+	}
+}
